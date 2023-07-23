a87039977e16d7e9f22ddfec85968c6ca52d2944
chiperpol <chiperpol.97@gmail.com>
2023-07-24 00:17:02 +0700

    fixing sidebar

--- resources/views/templates/partial/sidebar.blade.php
+++ resources/views/templates/partial/sidebar.blade.php
@@ -34,7 +34,7 @@
         </li>
 
         <li class="sidebar__nav-item">
-            <a href="{{ route('movies.index') }}" class="sidebar__nav-link {{ Request::is('movie-app/movies') ? 'sidebar__nav-link--active' : '' }}">
+            <a href="{{ route('movies.index') }}" class="sidebar__nav-link {{ Request::is('movies') ? 'sidebar__nav-link--active' : '' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M10,13H3a1,1,0,0,0-1,1v7a1,1,0,0,0,1,1h7a1,1,0,0,0,1-1V14A1,1,0,0,0,10,13ZM9,20H4V15H9ZM21,2H14a1,1,0,0,0-1,1v7a1,1,0,0,0,1,1h7a1,1,0,0,0,1-1V3A1,1,0,0,0,21,2ZM20,9H15V4h5Zm1,4H14a1,1,0,0,0-1,1v7a1,1,0,0,0,1,1h7a1,1,0,0,0,1-1V14A1,1,0,0,0,21,13Zm-1,7H15V15h5ZM10,2H3A1,1,0,0,0,2,3v7a1,1,0,0,0,1,1h7a1,1,0,0,0,1-1V3A1,1,0,0,0,10,2ZM9,9H4V4H9Z"/>
                 </svg>
@@ -43,7 +43,7 @@
         </li>
 
         <li class="sidebar__nav-item">
-            <a class="sidebar__nav-link {{ Request::is('movie-app/negara') ? 'sidebar__nav-link--active' : '' }}" data-toggle="collapse" href="#collapseMenu" role="button" aria-expanded="false" aria-controls="collapseMenu">
+            <a class="sidebar__nav-link {{ Request::is('master/negara', 'master/genre') ? 'sidebar__nav-link--active' : '' }}" data-toggle="collapse" href="#collapseMenu" role="button" aria-expanded="false" aria-controls="collapseMenu">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M19,5.5H12.72l-.32-1a3,3,0,0,0-2.84-2H5a3,3,0,0,0-3,3v13a3,3,0,0,0,3,3H19a3,3,0,0,0,3-3V8.5A3,3,0,0,0,19,5.5Zm1,13a1,1,0,0,1-1,1H5a1,1,0,0,1-1-1V5.5a1,1,0,0,1,1-1H9.56a1,1,0,0,1,.95.68l.54,1.64A1,1,0,0,0,12,7.5h7a1,1,0,0,1,1,1Z"/>
                 </svg>
@@ -55,7 +55,7 @@
 
             <ul class="collapse sidebar__menu" id="collapseMenu">
                 <li><a href="{{ route('negara.index') }}" class="{{ Request::is('master/negara') ? 'active' : '' }}">Negara</a></li>
-                <li><a href="{{ route('genre.index') }}">Genre</a></li>
+                <li><a href="{{ route('genre.index') }}" class="{{ Request::is('master/genre') ? 'active' : '' }}">Genre</a></li>
             </ul>
         </li>
     </ul>
