4d5f932274d1f0e84147e02c589a5daa469a8bc3
chiperpol <chiperpol.97@gmail.com>
2023-07-23 19:13:48 +0700

    update routing group and master genre

--- resources/views/templates/partial/sidebar.blade.php
+++ resources/views/templates/partial/sidebar.blade.php
@@ -25,7 +25,7 @@
     <!-- sidebar nav -->
     <ul class="sidebar__nav">
         <li class="sidebar__nav-item ">
-            <a href="{{ route('dashboard.index') }}" class="sidebar__nav-link {{ Request::is('movie-app/dashboard') ? 'sidebar__nav-link--active' : '' }}">
+            <a href="{{ route('dashboard.index') }}" class="sidebar__nav-link {{ Request::is('dashboard') ? 'sidebar__nav-link--active' : '' }}">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path d="M20,8h0L14,2.74a3,3,0,0,0-4,0L4,8a3,3,0,0,0-1,2.26V19a3,3,0,0,0,3,3H18a3,3,0,0,0,3-3V10.25A3,3,0,0,0,20,8ZM14,20H10V15a1,1,0,0,1,1-1h2a1,1,0,0,1,1,1Zm5-1a1,1,0,0,1-1,1H16V15a3,3,0,0,0-3-3H11a3,3,0,0,0-3,3v5H6a1,1,0,0,1-1-1V10.25a1,1,0,0,1,.34-.75l6-5.25a1,1,0,0,1,1.32,0l6,5.25a1,1,0,0,1,.34.75Z"/>
                 </svg>
@@ -54,8 +54,8 @@
             </a>
 
             <ul class="collapse sidebar__menu" id="collapseMenu">
-                <li><a href="{{ route('negara.index') }}" class="{{ Request::is('movie-app/negara') ? 'active' : '' }}">Negara</a></li>
-                <li><a href="edit-user.html">Edit user</a></li>
+                <li><a href="{{ route('negara.index') }}" class="{{ Request::is('master/negara') ? 'active' : '' }}">Negara</a></li>
+                <li><a href="{{ route('genre.index') }}">Genre</a></li>
             </ul>
         </li>
     </ul>
--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,24 @@ use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\MasterGenreController;
 use App\Http\Controllers\MasterNegaraController;
 use App\Http\Controllers\MoviesController;
 
-Route::get('/', function () {
-    return redirect('/movie-app/dashboard');
+Route::redirect('/', '/dashboard');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+Route::get('/movies', [MoviesController::class, 'index'])->name('movies.index');
+
+Route::controller(MasterNegaraController::class)->prefix('master')->group(function () {
+    Route::get('/negara', 'index')->name('negara.index');
+    Route::get('/negara/add', 'add')->name('negara.add');
+    Route::post('/negara/add', 'store');
+    Route::get('/negara/{id}/edit', 'edit')->name('negara.edit');
+    Route::put('/negara/edit/{id}', 'update');
 });
 
-Route::get('/movie-app/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
-Route::get('/movie-app/movies', [MoviesController::class, 'index'])->name('movies.index');
-Route::get('/movie-app/negara', [MasterNegaraController::class, 'index'])->name('negara.index');
-Route::get('/movie-app/negara/add', [MasterNegaraController::class, 'add'])->name('negara.add');
-Route::post('/movie-app/negara/add', [MasterNegaraController::class, 'store'])->name('negara.store');
-Route::get('/movie-app/negara/{id}/edit', [MasterNegaraController::class, 'edit'])->name('negara.edit');
-Route::put('/movie-app/negara/edit/{id}', [MasterNegaraController::class, 'update'])->name('negara.update');
+
+Route::controller(MasterGenreController::class)->prefix('master')->group(function () {
+    Route::get('/genre', 'index')->name('genre.index');
+});
